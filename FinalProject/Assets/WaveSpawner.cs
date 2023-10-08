9341ee426024fddae54c2f794d09b10f84e07bc9
Pedro <pedroa.moreno10@gmail.com>
2023-10-08 18:15:18 -0500

    Wave system prototype
    Implemented a wave system, need to tweak it and add text to let the player know when wave is completed. Also allow player to choose 1 of 3 items after completing a wave

---
+++ FinalProject/Assets/WaveSpawner.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Wave
+{
+    public string waveName;
+    public int numOfEnemies;
+    public GameObject[] typeOfEnemies;
+    public float spawnInterval;
+}
+public class WaveSpawner : MonoBehaviour
+{
+    [SerializeField] Wave[] waves;
+    [SerializeField] Transform[] spawnPoints;
+
+    private Wave currentWave;
+    private int currentWaveNumber;
+    private float nextSpawnTime;
+
+    private bool canSpawn = true;
+    
+
+    private void Update()
+    {
+        currentWave = waves[currentWaveNumber];
+        SpawnWave();
+        GameObject[] totalEnemies = GameObject.FindGameObjectsWithTag("Enemies");
+        if(totalEnemies.Length == 0 && !canSpawn)
+        {
+            currentWaveNumber++;
+            canSpawn = true;
+        }
+    }
+
+    void SpawnWave()
+    {
+        if (canSpawn && nextSpawnTime < Time.time)
+        { 
+            GameObject randomEnemy = currentWave.typeOfEnemies[Random.Range(0, currentWave.typeOfEnemies.Length)];
+            Transform randomPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+            Instantiate(randomEnemy, randomPoint.position, Quaternion.identity);
+            currentWave.numOfEnemies--;
+            nextSpawnTime = Time.time + currentWave.spawnInterval;
+            if(currentWave.numOfEnemies == 0)
+            {
+                canSpawn = false;
+            }
+
+        }
+    }
+
+}
